a8be486b3b9d67ed7d3daa80b876f5a6e5407fb6
Hiago Pilar Ribeiro <127118134+hiagoPilar@users.noreply.github.com>
2025-06-17 15:03:57 +0100

    .

--- exercicios/exe004.php
+++ exercicios/exe004.php
@@ -10,11 +10,45 @@
 <?php 
 
 // A - Ler um vector de N números inteiros e escrevê-lo pala mesma ordem.
-
+/*
 $num = [1, 2, 3, 4, 5];
 foreach ($num as $n){
     echo $n . " ";
 }
+*/
+
+// B - Ler um vector de N e escrevê-lo pala ordem inversa.
+/*
+$num = [1,2,3,4,5];
+for ($i = count($num) - 1; $i >= 0; $i--) {
+    echo $num[$i] . " ";
+}
+*/
+
+
+// C - Dado um vector de N números inteiros, determinar o maior e o menor elemento assim como as respectivas posições.
+
+$num = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+$bigger = $num[0];
+$smaller = $num[0];
+$biggerPos = 0;
+$smallerPos = 0;
+
+foreach ($num as $index => $value){
+
+    if($value > $bigger){
+        $bigger = $value;
+        $biggerPos = $index;
+    }
+    if($value < $smaller){
+        $smaller = $value;
+        $smallerPos = $index;
+    }
+}
+
+echo"The biggest number is $bigger at position $biggerPos.<br>";
+echo"The smallest number is $smaller at position $smallerPos.<br>";
+
 
 ?>
 </body>
