fc63fbf64d5d896d01af4250e67ec98015bd20df
Hiago Pilar Ribeiro <127118134+hiagoPilar@users.noreply.github.com>
2025-06-17 14:23:19 +0100

    .

---
+++ exercicios/exe004.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficha 04</title>
+</head>
+<body>
+    
+<?php 
+
+// A - Ler um vector de N números inteiros e escrevê-lo pala mesma ordem.
+
+$num = [1, 2, 3, 4, 5];
+foreach ($num as $n){
+    echo $n . " ";
+}
+
+?>
+</body>
+</html>
