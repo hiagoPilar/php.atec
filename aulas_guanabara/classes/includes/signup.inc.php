8560fac62676c2cb761114cb0523786b8b1627a4
Hiago Pilar Ribeiro <127118134+hiagoPilar@users.noreply.github.com>
2025-06-23 13:11:18 +0100

    .

---
+++ aulas_guanabara/classes/includes/signup.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === "POST"){
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+
+    require_once "../Dbh.php";
+    require_once "../Signup.php";
+
+
+    $signup = new Signup($username, $pwd);
+    $signup->signupUser();
+}
--- aulas_guanabara/classes/index.php
+++ aulas_guanabara/classes/index.php
@@ -8,13 +8,22 @@
 <body>
 
 
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username">
+        <input type="password" name="pwd">
+        <button>Signup</button>
+    </form>
+    
     <?php 
     
+    /*
     require_once 'Car.php';
 
     $car01 = new Car("Volvo", "Green", "XC90");
 
     echo $car01->getCarInfo() . "<br>"; // vai exibir a informação do carro 01
+    */
+
     
     ?>
     
