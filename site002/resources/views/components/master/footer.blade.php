d562fdc6394e072a45145a5273ead23405293ed7
hiago <hiago@gmail.com>
2025-07-14 21:15:25 +0100

    .

---
+++ site002/resources/views/components/master/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="text-center mt-5">
+    <p>Website by Luan Rodrigues using <a href="https://getbootstrap.com/" class="text-primary text-decoration-none">Bootstrap</a></p>
+</div>
---
+++ site002/resources/views/components/players/players-list.blade.php
@@ -0,0 +1,27 @@
+<h1>Players List</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nmae</th>
+      <th scope="col">Address</th>
+      <th scope="col">Retired</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  @foreach ($players as $player)
+  <tbody>
+    <tr>
+      <th scope="row">{{ $player->id }}</th>
+      <td>{{ $player->name }}</td>
+      <td>{{ $player->address }}</td>
+      <td>{{ $player->retired }}</td>
+      <td>
+        <button type="button" class="btn btn-success">Show</button>
+        <button type="button" class="btn btn-primary">Edit</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+      </td>
+    </tr>
+  </tbody>
+    @endforeach
+</table>
